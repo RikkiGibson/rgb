3766703aeca4eb7bdb977c4afed1c6bf4a46de18
Rikki Gibson <rikkigibson@gmail.com>
2018-02-19 14:41:16 -0800

    Add mmu, z80 files

---
+++ src/mmu.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+class MMU {
+  public:
+    uint8_t rb(uint16_t addr)
+    {
+        return 0;
+    }
+
+    uint16_t rw(uint16_t addr)
+    {
+        return 0;
+    }
+
+    void wb(uint16_t addr, uint8_t value)
+    {
+
+    }
+
+    void ww(uint16_t addr, uint8_t value)
+    {
+
+    }
+};
